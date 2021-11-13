c951555f3db0ddbc58048156ba129ad1f65de9cf
Petr Enin <petr.enin@gmail.com>
2021-11-13 19:21:38 +0100

    JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,50 @@ console.log('App.js is running...');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info...'
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Petr'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        React.createElement(
+            'b',
+            null,
+            '123'
+        )
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        React.createElement(
+            'b',
+            null,
+            'Prague'
+        )
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 console.log('App.js is running...');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info...</p>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Petr</h1>
+        <p>Age: <b>123</b></p>
+        <p>Location: <b>Prague</b></p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
